7a9502910efecadc2fbb037bef0e1a986df705bd
Agrim Asthana <agrimasthana@gmail.com>
2017-10-23 23:09:04 -0500

    performance improvements and textacy features

--- api.py
+++ api.py
@@ -1,24 +1,29 @@
 import falcon
 import json
-from nltk import tokenize
+import textacy
 from spacy.en import English
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 class NLP(object):
+    sid = SentimentIntensityAnalyzer()
+    spc = English()
+
     def on_post(self, req, resp):
-        sid = SentimentIntensityAnalyzer()
-        spc = English()
         try:
             raw_json = req.stream.read()
-            input = json.loads(raw_json, encoding='utf-8')
+            input = json.loads(raw_json, encoding='utf-8')['sentence']
         except Exception as ex:
             raise falcon.HTTPError(falcon.HTTP_400,
                                    'Error',
                                    ex.message)
         resp.status = falcon.HTTP_200
-        op = sid.polarity_scores(tokenize.sent_tokenize(input['sentence'])[0])
-        op['subjects'] = [t.text for t in spc(unicode(input['sentence'])) if (t.dep_ == "nsubj")]
+        op = self.sid.polarity_scores(input)
+        op['subjects'] = [t.text for t in self.spc(unicode(input)) if (t.dep_ in ["nsubj", "nsubjpass", "dobj", "iobj", "pobj"])]
         resp.media = op
+
+        # Future Batch processing function
+        # print [x for x in textacy.keyterms.singlerank(textacy.Doc(unicode(input), lang=u'en'))]
+
 api = falcon.API()
 api.add_route('/parse', NLP())
